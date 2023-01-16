62c3af51c053f9ac3596725dd391f62f76440e07
Rohit Kumar <rohit09619@gmail.com>
2023-01-16 19:50:11 +0530

    first try solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        // obj.name = "accio";  // gives error
+        obj.setter("Accio");
+        System.out.println( obj.getter());
+    }
   
 }
